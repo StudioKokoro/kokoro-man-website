14fa90f3a9e4ceb96cd3c7df94bbfcf43e7d0fb2
StudioKokoro <sankerg2020@gmail.com>
2026-06-17 11:43:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,24 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
+const routes = {
+  '/sitemap.xml': { file: 'sitemap.xml', type: 'application/xml; charset=utf-8' },
+  '/robots.txt': { file: 'robots.txt', type: 'text/plain; charset=utf-8' },
+};
+
 const server = http.createServer((req, res) => {
-  const filePath = path.join(__dirname, 'index.html');
+  const route = routes[req.url];
+  const fileName = route ? route.file : 'index.html';
+  const contentType = route ? route.type : 'text/html; charset=utf-8';
+
+  const filePath = path.join(__dirname, fileName);
   fs.readFile(filePath, (err, data) => {
     if (err) {
-      res.writeHead(500);
-      res.end('Error loading page');
+      res.writeHead(404);
+      res.end('Not found');
       return;
     }
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.writeHead(200, { 'Content-Type': contentType });
     res.end(data);
   });
 });
